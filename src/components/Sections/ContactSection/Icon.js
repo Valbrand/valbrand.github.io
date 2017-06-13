c1f96d7dbbf8e44643307f70feb8236d639f0734
Daniel Oliveira <dvalbrand@gmail.com>
2017-06-13 17:16:57 -0300

    Adding icon link list

---
+++ src/components/Sections/ContactSection/Icon.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Icon = ({ link }) => {
+  const {
+    icon,
+    link: url,
+    color
+  } = link
+
+  return (
+    <Root
+      href={url}
+      target='_blank'
+      color={color}
+    >
+      <IconElement className={`fa fa-${icon} fa-2x`} />
+    </Root>
+  )
+}
+
+const Root = styled.a`
+  color: inherit;
+
+
+`
+
+const IconElement = styled.span`
+  opacity: 0.6;
+
+  cursor: pointer;
+
+  transition: opacity 0.25s, transform 0.25s;
+
+  &:hover {
+    opacity: 1;
+
+    transform: scale(1.1, 1.1);
+  }
+`
+
+export default Icon
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import './index.css'
+import './css/index.css'
+import './css/font-awesome.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
